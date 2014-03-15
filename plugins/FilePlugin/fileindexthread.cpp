01fca0e8a1c69ecb66af1267c24bc5a0650a4477
Jason Eslick <jslick@gmail.com>
2014-03-14 22:26:14 -0500

    Add file index paths for Windows

--- plugins/FilePlugin/fileindexthread.cpp
+++ plugins/FilePlugin/fileindexthread.cpp
@@ -6,8 +6,25 @@
 #include <QDir>
 #ifdef Q_OS_WIN
 #  include <QSettings>
+#  include <userenv.h>
+#  include <Shlobj.h>
 #endif
 #include <QReadWriteLock>
+
+static QString expandEnvironmentPath(const QString& path)
+{
+#ifdef Q_OS_WIN
+    WCHAR dst[512];
+    DWORD nCopied = ExpandEnvironmentStringsW(path.toStdWString().c_str(), dst, sizeof(dst));
+    if (nCopied == 0)   // failure
+        return path;
+    const QString expanded = QString::fromWCharArray(dst);
+    return expanded;
+#else
+    // TODO (wordexp)
+    return path;
+#endif
+}
 
 FileIndexThread::FileIndexThread(FileCatalogDb& db, QReadWriteLock& dbLock, QObject* parent) :
     QThread(parent),
@@ -31,20 +48,31 @@ void FileIndexThread::run()
         { QDir(QDir::homePath()).relativeFilePath("Desktop"), true, { "*.desktop" } },
     };
 #elif defined(Q_OS_WIN)
-    static const QList<PathToIndex> pathsToIndex = {
+    QList<PathToIndex> pathsToIndex = {
         { QSettings("HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders", QSettings::NativeFormat).value("Common Desktop").toString(), true, { "*.lnk" } },
-        { QSettings("HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders", QSettings::NativeFormat).value("Common Desktop").toString(), true, { "*.lnk" } },
-        { QSettings("HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders", QSettings::NativeFormat).value("Common Start Menu").toString(), true, { "*.lnk" } },
         { QSettings("HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders", QSettings::NativeFormat).value("Common Start Menu").toString(), true, { "*.lnk" } },
     };
+
+    {
+        // There are newer API functions for this; but they are only for Vista and newer
+        WCHAR szPath[MAX_PATH];
+        HRESULT hr = SHGetFolderPathW(0, CSIDL_STARTMENU, 0, 0, szPath);
+        if (SUCCEEDED(hr))
+            pathsToIndex.push_back({ QString::fromWCharArray(szPath), true, { "*.lnk" } });
+
+        hr = SHGetFolderPathW(0, CSIDL_DESKTOPDIRECTORY, 0, 0, szPath);
+        if (SUCCEEDED(hr))
+            pathsToIndex.push_back({ QString::fromWCharArray(szPath), true, { "*.lnk" } });
+    }
 #else
 #error "Not implemented for this operating system"
 #endif
 
     for (const PathToIndex& pathInfo : pathsToIndex)
     {
+        const QString pathString = expandEnvironmentPath(pathInfo.path);
         QStack<QString> dirstack;
-        dirstack.push_back(pathInfo.path);
+        dirstack.push_back(expandEnvironmentPath(pathString));
 
         while (dirstack.empty() == false)
         {
